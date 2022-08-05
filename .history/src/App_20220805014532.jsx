0189cb6142be5ee89388161bba6a28970530f463
Echooooo-zhn <haonanZHONG17@outlook.com>
2022-08-06 01:32:02 +1000

    hihi

---
+++ .history/src/App_20220805014532.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import StudentMain from './page/StudentMain';
+import ExpertMain from './page/ExpertMain';
+import StudentProfile from './page/StudentProfile';
+import ExpertProfile from './page/ExpertProfile';
+import Login from './page/Login';
+import StudentRegister from './page/StudentRegister.jsx';
+import StudentQAList from './page/StudentQAList.jsx';
+import QACategory from './page/QACategory'
+import ExpertAva from './page/ExpertAva.jsx';
+import StudentFindExpert from './page/StudentFindExpert.jsx';
+import QAadd from './page/QAadd.jsx';
+import QuestionThread from './page/QuestionThread.jsx';
+import ExpertPostRecord from './page/ExpertPostRecord.jsx';
+import ReviewPost from './page/ReviewPost.jsx';
+import StudentQuestionThread from './page/StudentQuestionThread.jsx';
+import AddExpert from './page/AddExpert.jsx';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/student_register" element={<StudentRegister />} />
+        <Route path="/student_main" element={<StudentMain />} />
+        <Route path="/expert_main" element={<ExpertMain />} />
+        <Route path="/student_profile" element={<StudentProfile />} />
+        <Route path="/expert_profile" element={<ExpertProfile />} />
+        <Route path="/student_QA_list" element={<StudentQAList />} />
+        <Route path="/QACategory" element={<QACategory />} />
+        <Route path="/QACategory/:categoryid" element={<QACategory />} />
+        <Route path="/expert_ava" element={<ExpertAva />} />
+        <Route path="/student_main/find_expert" element={<StudentFindExpert />} />
+        <Route path="/qaAdd" element={<QAadd />}></Route>
+        <Route path="/question_thread" element={<QuestionThread />}> </Route>
+        <Route path="/student_question_thread" element={<StudentQuestionThread />}> </Route>
+        <Route path="/expert_post_record" element={<ExpertPostRecord />}> </Route>
+        <Route path="/review_post" element={<ReviewPost />}> </Route>
+        <Route path="/addExpert" element={<AddExpert />}> </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20220806013055.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Echooooo-zhn haonanZHONG17@outlook.com
+ * @Date: 2022-08-06 00:56:50
+ * @LastEditors: Echooooo-zhn haonanZHONG17@outlook.com
+ * @LastEditTime: 2022-08-06 01:30:54
+ * @FilePath: \amplifyapp\src\App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+
+
+function App() {
+  return (
+    <h1>Hihi</h1>
+  );
+}
+
+export default App;
